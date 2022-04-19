7e81e71273f4468d75c39d06a3b6771c2d6f6b7e
aldo <aldoexpert@gmail.com>
2022-04-19 23:22:33 +0700

    Update

--- app/modules/aka_keuangan/journal_set/account_form_2.php
+++ app/modules/aka_keuangan/journal_set/account_form_2.php
@@ -4,12 +4,12 @@
 // --------------------------
 if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
 ?>
-HR+cPmOz0LFX6FfaZ8QKSCZC79V23JyY82+EI+P5ndoMHAWfyLwYqHG3AdNAT7FygsHuE/Kva/oS
-NKzzTPi6KAiO4/zrb4QCm/pEKpjxNuZioAD4mDIOsbeuziKIjFnnUMVB/L1OR1r+D8vkPUraXwLx
-3VqBMEiWs7/fQE7opnit/aASjUboeKpE4vYRRQ6FJgn+yZHMt+Eiw1yJvf/mZmVsNa4L6u+8woQi
-v4yg04y2E7htquOn3roZSmVo7N/qKKpdgIg4J77/RSxliQV0DnukaTCUB5Kxud1JBZLKXAWiWQO+
-IWNbLR9m5ShNJ8U3IZn7/OZKdk1PtmV9K0yW6C95w5fZ546P63u4+6er2Kchi8Rz17tEbZkmChUP
-RNY7NhG7NbmByhxq/3HqdDHYQQWuqWsCUlbWfT7SmMxOGjgOpwJCKP4jDReEYXZHwtS8JLWrbtBN
-FcbrDHL56uqK2uUWRdOThof8macBaR+wagmZ3JHUVY7IdlHhkoVV41tYceVi8Pq+TlnqX1csZWEL
-PwYG+wXLZWW4SngWfEJB/vc9q33I654ZHB7jb5LSnW0TdYYTYdwbEnrmrXGQ+FmPM+0e35ThbS2e
-pykyES9yB5l5UJkhTvpHjeweU6j3UG==
+HR+cPwvXzMo8jyr39TfWJpBW3je+OFlhiaoAWOF8jupFlCDPvdobLNvVHyDjnrYzfUufzceTHAYK
+mNUbamKMx5inUDcgVST0caE/DaWqJg9U95QcaLZ95qul+YArCTz8CfrgFSOepwNihk76ZNGmk8WW
+Lx8dRaPUQQF1d9PAIx0JVVingtqmbLgE3mgWRiCIkDEYHdFIbdktSZCGv6VytOSN7A6PS4CPJeRR
++VVBtVCQ4G21LOASTrxdVRLsgQSxIwEw2+kaPGl9aHBY1VH+nIK8fWsmWDE8Z3rPS0CvrIbcxsuJ
+oUT3SGjAj/Q1Nt3Xt6c4WDGd4ciTHYNWL95gR8QfMG7NtNBwXAMTplhWX5ww0Bb4/lBMyJBA2Zlv
+FRufpg9Sg5FiYY+/P4rqc9EOT8SSwNmauB9byF5CQUa167HBedaX1Gu4AS2Bq8ZSExjcnEW8KJV9
+kMRbCeq7ORkKQTm2zP8M5tFP7wgSnmVRT+0eIfArQAk+xJBNN8rsKcYq6ptGI/J0Cd6DvEPuW8Iw
+v20U3KTT1MP3N7BFicBeXo032dpWenFP5CHYEGY+TdmDlTq9aEcBULlfLuL/cJCc2ZPqmxC+Nj1n
+PYcLETYerkbgMm6woVVVNu8thmXnE4y=
--- system/libraries/external/google-api-php-client--PHP7.4/vendor/google/apiclient-services/src/Cloudchannel/GoogleCloudChannelV1CommitmentSettings.php
+++ system/libraries/external/google-api-php-client--PHP7.4/vendor/google/apiclient-services/src/Cloudchannel/GoogleCloudChannelV1CommitmentSettings.php
@@ -1,54 +1,60 @@
-<?php //00551
-// --------------------------
-// Created by Dodols Team
-// --------------------------
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
-?>
-HR+cPyjcnqbfv3jk1AfIQe0ZGdLeabQg+UKeeCodc6xFnuUzs7wtPjnOUgQ6kyo/kol9XPjCsBxh
-Xuh6qLSrhuRaUQ7LIhqC6G2rMy+4hGDDd197m+y8qso3AP8bvGSjnsB/eoLe/exxXdnqZXHmcG24
-ZcAC09QiyDdJ2L3oo6+jwPducus0f3SLNeSYaGJvxoVV9Lt59o5x47ve40UHzMhVE2cKjpFoA4ff
-xxUOQ31WNxXQBTytXXuPBNsvEUcLo1LHEhQR87ZdHqD3IyVHq0yBWSecu4X5t/ri4dM8v4+SlhQ+
-t/qXONCfb1lqkCvH+2QeobtnFIF/Bf3WfLYM4/0OKUyFZV1fjjTd3+WXsiXHrnnX79lNdCC94LUe
-jlxOhB08wQdr0T+U4ygHGF5B55d5X/hDxH+VhBy3ZG/sGjWuCl1z2DUaEJXzyCHjVC8FrbpdHCpY
-VASRQvmOaqgld4sQUkk691pma8soKHKV7JsHiMr59PJwV0Bfm285JNdWQbzfxpOl/nI9dL4VaIba
-15YOTd/5Vm1wOCiaRnlz4umV2l/yT3+1N6C/ZMy7IK+Qv+VwkR6nNn3XuhRR4vuGQKU6HsA5GFDH
-d80zUS3knPl9H669H979t6L9VqCKYn4XdGzJGT4tL4/5Vv+TnI9j+MqVSjCbA1562H8rBcuP1yqQ
-vbL95nIpHgPXwpY9l4GNLilPh5sb1FvA+onV0IkzXv4URRz2Qdc47Z4HcZ0J8WwwYJ8H6d8OqPR1
-qY6HgaF2iajVIEanz3818Lacg1wXz8a00mtmezPXRBZmpCd+EpETOT+y968saYiTUW4D9+PjCDRr
-Jj3rilBp9+NXKqQPDtvFf+ee7riRVglpf2iW4LqcJ2aIwgYgv0fcSpVCvU2424h0LHlEjvhUWbel
-r+1/FV3fBNCmKQcpO6h9gQi4bUw/oh/ftsZk0gnWaVpVi/+q+mrAXm4aJNRJuJiVkHp92s65rEUU
-letFdW1Q1PMldbasoDIb//1Oj6LEy6zlKEWERbPG1n1gzJ0uD/MGVr3tX4QV0rBnb4mzVcTofBGu
-RrqWPCNx3aMgSxcNuIfd6un39lZ1b5MJWApL8fR5vQSU2gzNZs9qVC+1hr2l58A56PC4WGLOQd4P
-WBboGLVCSXqQd19Oe0h5lSS6AlsyOQ9zejYzzUQjjHFHV+GGuaCYxycoyojUOvr31MvM44BCkniJ
-T+TVUFUathZO14KTeIOGjlPoyzdrbjy7xM439oPIVKcVlXth9JypVB4Shyz4hV7Bg0FLFyiqSB9i
-43g/qP9YpKWqireBCHnNtc/U+JdPSB1hhnrkVxhHhkNb0KzgSeiDFuC22vjHtkeQVwLUyYQ66Oku
-tDPCrZB351iWN4k9igYfKVetrEO9P48F3OXeNpQodN4+uu6F3POeAnJpLjqvfbEsmOHpuUTbt9bo
-ucb7If+VHaGszU/ipc1zG2pBbY9JZDZC1/5WWR12cNQQMjx6K59hAJhBr6zqh0caa8CRU3AI1kk2
-rr5ri14RoAfFEY1eKQ5mBSr/IYgiAo+z+4RYsklHjZhuB+NFp2vkOIRAMZ9EYzbgG/RAzLMeG6v5
-Fm2pPrqwSuUqWnHWeMMZy74F+HKQHz3Zg805glJlakrAEzjTe59hjnGctsZOTsgd2z6WbhGK2pxr
-SWPk4+W+ILD/EGprxyi/DVIDQKYpvwkbjfgfHzDMUOgF9gIS0sj2cr8XZqfBl7WrE4TN4TiHN4kO
-3OStUWuftSVGko0WDyUVBHc/3W97ukwtf5Hhbdawvw+/D/p0Y66s9GUp7TDunRbRkFxl2b2rTdto
-SVXCIVnPZjIJCTo6EYy8BnR6XCexfGno3pECzHn0GOF9Q5fmMD8gN4p2/ypdKpCtjhSEc39PaxRx
-ED9ocvWFm5Bn+SFjdyCxKnUdfAli1pOsbmYsWmFrmzw2arnVX06JnupYB8DGtmeOHn4MMSpu+EmC
-hke33S7jGZ6JT9c86pOucSqv8zBx2ag+btGWh7HJxbpzTqJH2yGhSKMHEq4Lsr9KmPZWsGYj7RiQ
-iBosu5KXvuMTT3J9iPCcvHoDuC6dPNiemneEhvYEyDqv2Z3ifIFpIJYv1ouLlJEMzcXZwG2ne92/
-G3JIVUM24R6zlUxaE29VkAac8fh9iZ0VR1qfP2G2+Dj6U46coICHGObU6SvhRt4ukSmkj6FEciJe
-tcnewlvcjb9T/bfzppHYIMVI7Eob/XV2TIAEciKfwQTedtc919ixzBc9iKPiJmUuY36hbCJxvi0l
-UIcrXkUYDDMCGMYTi5DwzRlJq6hBfi58pY6y79Obl6G8qL6Tec5n93cpYaxzyNEyTaX6k56NJDmt
-hP9Bl8Q2WvJiDKYPPYppDBg6ZNOPeVyufcCdtWuBMK7/kkoCI9nq3MXQZSiM/1N/Yln8YmEY3TZr
-GP0OGBWo9WCXojKDTvVo/mGYRNWQrrkWFjNVVDuXZCLCOx+7GKsI0b4Yg5Vbaf/3n3qu6SZ7PZR+
-PDgg9Sf4AQ2fXyVCRQ5OrenoY04TtO1AvAipPq3mE4mZed+Wr46QZO/9Pq0uu2W3fSPIwQ3tSM0V
-nhnFSz1hJrKpK4ZDQbv3e7KUo3yaRpXkadG3vOSrohNZQ9XNxLo1kqJ63vxgsaQyZdJGmph6Ps88
-VQoWvDvg22H71RP58e3VuhCYN+VkfzxyXuYfzlRQqrBdvXDkQ5Fd6HbIsM34PVm4FVPNU8LxTt1a
-iUeigDJJO3U+TyeOUPoH/B0RUaizJK6l1rD9Khw4qlb4VPyfmsQjZHr0cgcKkOOr2PoNxtYD3ryj
-Zi/bYjuFuxVmIuJ1a0Ejy9SgICY6ZXmxT3ypxVEoaBZPK0Gz9jgQYW9qsM48lYSCn84uyng66cm3
-Oil8sG9vjT8QqtRh5b8zDqaOj1x1k9jd8pY+6QNuxkLYVpDWzkhsgs+GHvkNsKDwNi2rb56Cih5S
-8GAoFaUpF/xGHxCbM7dVxEXS74wJiayUGhOYCBU/lSf8RptJKEaGUSq4w2U2m0C+m/TVH888Jopf
-yMrkYfSiyS0N1foumP7EDAtSr7DciY8PxPjrtFDOdiz2uQjgkzaZoaOtZJ9+rdTc21ZaHvCF/tFR
-Vea9xNkCZoRGeXljeDiC8kKZDA/BSaN9flWSlVEXMpjSS65nXS7xE4wnu1SqNMbSU/5QCT5zMpxm
-4dlyNcbcSJZLPEqbCh7bR9e+1aH5dnttmtI39j4Uxrd5djKBdpaI2qeKw9zOqGsBMc2EL6KTnf96
-lDg/JNIjBo1mN0y35XmoFmqd9lYBrsNtWC8IaQRzb2B+H/zsXOA1J+TWVCZDadtj0tLjH2i8k5QD
-PtBgrRYOoHpmboF843IzRxzWus1nNQHcx2mTanqWEVrZsDXNdk13abI9jmZB7oft9cjtyPy5FZv8
-VwXquVAMa0XHDDd+nzJmq1Sqr91Kpc3xoprsf6QmAJUO7IcXUPbxKMkGk2N7oO07BZOCdQYR6K3/
-FUO8YOwt/w+Iok0H0VJhLr/koBWG7nFb8wCorOkf54luaa3AHSOJX1Jq4x92aXzndX6xAwq9jPyE
-MQ0d407cwn19dhW7VJZxlRKm8sbL69GWaUv8S2X29gwWjhGz
+<?php
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+namespace Google\Service\Cloudchannel;
+
+class GoogleCloudChannelV1CommitmentSettings extends \Google\Model
+{
+  public $endTime;
+  protected $renewalSettingsType = GoogleCloudChannelV1RenewalSettings::class;
+  protected $renewalSettingsDataType = '';
+  public $startTime;
+
+  public function setEndTime($endTime)
+  {
+    $this->endTime = $endTime;
+  }
+  public function getEndTime()
+  {
+    return $this->endTime;
+  }
+  /**
+   * @param GoogleCloudChannelV1RenewalSettings
+   */
+  public function setRenewalSettings(GoogleCloudChannelV1RenewalSettings $renewalSettings)
+  {
+    $this->renewalSettings = $renewalSettings;
+  }
+  /**
+   * @return GoogleCloudChannelV1RenewalSettings
+   */
+  public function getRenewalSettings()
+  {
+    return $this->renewalSettings;
+  }
+  public function setStartTime($startTime)
+  {
+    $this->startTime = $startTime;
+  }
+  public function getStartTime()
+  {
+    return $this->startTime;
+  }
+}
+
+// Adding a class alias for backwards compatibility with the previous class name.
+class_alias(GoogleCloudChannelV1CommitmentSettings::class, 'Google_Service_Cloudchannel_GoogleCloudChannelV1CommitmentSettings');
